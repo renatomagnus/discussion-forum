7d0c7ec351b3afbd76e5d0e09d3955b7124d6acd
Renato Magnus <rmagnusdarosa@gmail.com>
2024-10-07 14:28:50 -0300

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
 
 export default async function Home() {
-  const session = await auth();
 
   return (
    <div>
-    <form action={actions.signIn}>
-    <Button type="submit">signIn!</Button>
-    </form>
-    <form action={actions.signOut}>
-    <Button type="submit">signOut!</Button>
-    </form>
-    {
-      session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>
-    }
-    <Profile />
+   Home Page
    </div>
   )
 }
