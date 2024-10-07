b3c55f6a0986fca2e577868b9ea5176e9452f3ce
Renato Magnus <rmagnusdarosa@gmail.com>
2024-10-07 16:30:59 -0300

    feat: creating form errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import TopicCreateForm from "@/components/topics/topic-create-form";
 
 export default async function Home() {
-
   return (
-   <div>
-   Home Page
-   </div>
-  )
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
 }
