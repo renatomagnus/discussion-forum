9343f2495a66b7317069e62e359312b30d44b3a9
Renato Magnus <rmagnusdarosa@gmail.com>
2024-10-08 14:20:50 -0300

    request memoization

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,11 +1,13 @@
 import type { Comment } from "@prisma/client";
 import { db } from "@/db";
+import { cache } from 'react';
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId( postId: string): Promise<CommentWithAuthor[]> {
+export const fetchCommentsByPostId = cache(( postId: string): Promise<CommentWithAuthor[]> => {
+  console.log('making a query')
   return db.comment.findMany({
     where: { postId },
     include: {
@@ -17,4 +19,4 @@ export function fetchCommentsByPostId( postId: string): Promise<CommentWithAutho
       }
     }
   })
-}
+})
